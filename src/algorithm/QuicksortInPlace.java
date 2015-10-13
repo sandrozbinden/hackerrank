390a5db390beb8b24f1626bd5920a3de563f6b78
sandrozbinden@gmail.com <sandrozbinden@gmail.com>
2015-10-13 08:44:28 +0200

    Format QuicksortInPlace

--- src/algorithm/QuicksortInPlace.java
+++ src/algorithm/QuicksortInPlace.java
@@ -19,33 +19,35 @@ import java.util.Scanner;
 public class QuicksortInPlace {
     public static class Solution {
 
-		public static void quicksort(int[] ar, int low, int high) {
-			if (low < high) {
-				int pivotIndex = partition(ar, low, high);
-				quicksort(ar, low, pivotIndex -1);
-				quicksort(ar, pivotIndex +1, high);
-			}
-		}
+        public static void quicksort(int[] ar, int low, int high) {
+            if (low < high) {
+                int pivotIndex = partition(ar, low, high);
+                quicksort(ar, low, pivotIndex - 1);
+                quicksort(ar, pivotIndex + 1, high);
+            }
+        }
+
         public static int partition(int[] ar, int low, int high) {
-        	int pivotValue = ar[high];
-        	int i = low;
-        	for (int j = low; j < high; j++) {
-        		if (ar[j] <= pivotValue) {
-        			swap(ar, i, j);
-        			i = i +1;
-        		}
-        	}
-        	swap(ar, i, high);
-        	printArrayLn(ar);
-        	return i;
+            int pivotValue = ar[high];
+            int i = low;
+            for (int j = low; j < high; j++) {
+                if (ar[j] <= pivotValue) {
+                    swap(ar, i, j);
+                    i = i + 1;
+                }
+            }
+            swap(ar, i, high);
+            printArrayLn(ar);
+            return i;
         }
 
         private static void swap(int[] ar, int i1, int i2) {
-        	int tempValue = ar[i1];
-        	ar[i1] = ar[i2];
-        	ar[i2] = tempValue;
-		}
-		static void printArrayLn(int[] ar) {
+            int tempValue = ar[i1];
+            ar[i1] = ar[i2];
+            ar[i2] = tempValue;
+        }
+
+        static void printArrayLn(int[] ar) {
             for (int n : ar) {
                 System.out.print(n + " ");
             }
@@ -59,7 +61,7 @@ public class QuicksortInPlace {
                 for (int i = 0; i < n; i++) {
                     ar[i] = in.nextInt();
                 }
-                quicksort(ar, 0, ar.length -1);
+                quicksort(ar, 0, ar.length - 1);
             }
         }
     }
